97a7942ca68efd4df9ba27f6c1fe05875ff5cd7f
Delilah1004 <pskdy8414@naver.com>
2020-09-04 16:26:14 +0900

    movie_id로 movieList 가져오기

--- SearchService/src/main/java/com/make/controller/SearchController.java
+++ SearchService/src/main/java/com/make/controller/SearchController.java
@@ -1,10 +1,16 @@
 package com.make.controller;
 
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.make.service.SearchService;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @RestController
 public class SearchController {
@@ -15,13 +21,19 @@ public class SearchController {
     @GetMapping("/search_year")
     public void moviesInYear(@RequestParam String year) throws Exception{
 
-        searchService.getMovies(year);
+        //searchService.getMovies(year);
     }
 
     @GetMapping("/search")
     public void getMovie(@RequestParam int num) throws Exception{
 
         searchService.getMovie(num);
+    }
+
+    @GetMapping("/test")
+    public String getPopularMovieListDesc(@RequestParam int pageNum) throws Exception {
+
+        return searchService.getPopularMovieListDesc(pageNum);
     }
 
 }
--- SearchService/src/main/java/com/make/service/SearchService.java
+++ SearchService/src/main/java/com/make/service/SearchService.java
@@ -3,7 +3,6 @@ package com.make.service;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
-import com.make.model.MoviePreView;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedReader;
@@ -15,17 +14,78 @@ import java.util.ArrayList;
 @Service
 public class SearchService {
 
-    public void getMovies(String year) throws Exception {
+    private static final String mainURL = "https://api.themoviedb.org/3";
+    private static final String imageURL = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
+    private static final String API_KEY = "334cc048cf91e9c7e784d8d3241e3b4c";
+    private static final String KOREAN = "ko-KR";
 
-        ArrayList<MoviePreView> movieList = getMoviesByYear(getLatestMovieId(), year);
+    public String getPopularMovieListDesc(int pageNum) {
 
-        for (MoviePreView movie : movieList) {
-            System.out.println("포스터 URL : " + movie.getPosterPath());
-            System.out.println("제목 : " + movie.getTitle());
-            System.out.println("날짜 : " + movie.getDate());
-            System.out.println("개요 : " + movie.getOverview());
-            System.out.println("\n--------------------------------------\n");
+        String URL = mainURL;
+        URL += "/discover/movie";
+        URL += "?api_key=" + API_KEY;
+        URL += "&language=" + KOREAN;
+        URL += "&sort_by=popularity.desc&page";
+        URL += "&page=" + pageNum;
+
+        return getJson(URL);
+    }
+
+    public String getJson(String URL) {
+
+        String json = null;
+
+        try {
+            URL url = new URL(URL);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+            json = br.readLine();
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+
+        return json;
+    }
+
+    public void printPreviewInfo(String json) throws Exception {
+
+        JsonParser parser = new JsonParser();
+        JsonElement obj = parser.parse(json);
+
+        String poster_path = obj.getAsJsonObject().get("poster_path").getAsString();
+        String title = obj.getAsJsonObject().get("title").getAsString();
+        String date = obj.getAsJsonObject().get("release_date").getAsString().substring(0, 4);
+        String overview = obj.getAsJsonObject().get("overview").getAsString();
+
+        System.out.println("포스터 : " + imageURL + poster_path);
+        System.out.println("제목 : " + title);
+        System.out.println("날짜 : " + date);
+        System.out.println("개요 : " + overview);
+    }
+
+    public String getMovieById(long n) throws Exception {
+
+        String URL = mainURL;
+        URL += "/movie/" + n;
+        URL += "?api_key=" + API_KEY;
+        URL += "&language=" + KOREAN;
+
+        BufferedReader br;
+        URL url;
+        HttpURLConnection conn;
+        String protocol = "GET";
+
+        url = new URL(URL);
+        conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod(protocol);
+        br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+        return br.readLine();
     }
 
     public void getMovie(int num) throws Exception {
@@ -35,9 +95,6 @@ public class SearchService {
         BufferedReader br;
         URL url;
         HttpURLConnection conn;
-
-        MoviePreView movie;
-        ArrayList<MoviePreView> movieList = null;
 
         for (int i = 2; i <= 80; i++) {
             URL = "https://api.themoviedb.org/3/movie/" + i + "?api_key=334cc048cf91e9c7e784d8d3241e3b4c&language=ko-KR";
@@ -61,16 +118,13 @@ public class SearchService {
         }
     }
 
-    public ArrayList<MoviePreView> getMoviesByYear(long lastId, String year) throws Exception {
+    public void getMoviesByYear(long lastId, String year) throws Exception {
 
         String URL, json, date;
         String poster_path, title, overview;
         BufferedReader br;
         URL url;
         HttpURLConnection conn;
-
-        MoviePreView movie;
-        ArrayList<MoviePreView> movieList = null;
 
         for (int i = 2; i <= 80; i++) {
             URL = "https://api.themoviedb.org/3/movie/" + i + "?api_key=334cc048cf91e9c7e784d8d3241e3b4c&language=ko-KR";
@@ -110,8 +164,6 @@ public class SearchService {
                 */
             }
         }
-
-        return movieList;
     }
 
     public ArrayList<String> getAllMovies(long lastId) throws Exception {
